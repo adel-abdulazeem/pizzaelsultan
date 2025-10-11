7482e8509a0b3eecc8bd337490f9a8c059ecaeaa
Adel <adel_abdulazeem@outlook.com>
2025-10-11 16:02:04 +0300

    first commit

---
+++ src/data/menus.js
@@ -0,0 +1,41 @@
+export const menuMain = [
+
+    {
+        name: "الرئيسية",
+        url: "/",
+        highlighted: false,
+    },
+    {
+        name: "المنيو",
+        url: "/menu",
+        highlighted: false,
+    }
+];
+
+export const footerPrimary = [
+    {
+        title: "Navigatie",
+        items: [
+            { name: "home", url: "/" },
+        ],
+    },
+];
+
+export const footerSecondary = [
+    {
+        title: "support",
+        items: [
+            { name: "pricing", url: "/pricing" },
+        ],
+    },
+];
+
+export const footerTertiary = [
+    {
+        title: "Links",
+        items: [
+            { name: "privacybeleid", url: '/privacy-policy' },
+            { name: "api status", url: "/api-status" },
+        ],
+    },
+];
